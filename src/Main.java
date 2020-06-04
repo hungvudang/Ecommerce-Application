0b4227473c6deacd7b156fe9bfea8f32cd4f79dc
hungv <hungv@DESKTOP-TV2SODT>
2020-06-04 16:01:40 +0700

    add class constants

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,8 @@
  * @author hungv
  */
 public class Main {
-    public static void main(String[] args) {
-        
-    }
+//    public static void main(String[] args) {
+//        
+//    }
     
 }
